107bea83531f9297c08da30a784de35a777b57a8
Akash <65025242+psychoAKASH@users.noreply.github.com>
2025-05-04 17:59:14 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 //import pdf from "../../Assets/../Assets/AKGenericResume.pdf";
-import pdf from "../../Assets/../Assets/Akash_Sep_wd_resume.pdf";
+import pdf from "../../Assets/../Assets/akash_mar_wd_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
